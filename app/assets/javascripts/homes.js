935270b1996d99cd55fbcb6a69bad1c64ec4460a
Jonathan Dallas <github@jonathandallas.com>
2013-06-02 14:47:57 -0400

    overlay shows data when name is clicked

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -7,6 +7,7 @@
 		CIDC.Map = (function() {
 
 			var currentfloor = 4;
+			var location_num;
 			
 			// Display info overlay
 			// parameters are
@@ -30,87 +31,39 @@ console.log(type);
 						$('.map').removeClass('fourth').addClass('third');
 					}
 					// populate the overlay
-					$('.map .overlay .data').html('<ul></ul>').
-						append('<li class="name">'+data.name+'</li>');
-						//<br />'+item.title+'<br />'+item.email+'<br />'+item.phone+'<br />Office#'+item.office);
+					var strHtml = '<ul>';
+
+					strHtml += '<li class="name">' + data.name + '</li>';
+					if(type == 'employees'){
+						strHtml += '<li class="title">' + data.title + '</li>';
+						strHtml += '<li class="email">' + data.email + '</li>';
+					}
+					strHtml += '<li class="phone">' + data.phone + '</li>';
+
+					$('.map .overlay .content').html(strHtml);
 					
-					// save the location_number
-					
+					// position the overlay
+					positionInfoOverlay(data.location_num);
+
+					// show the overlay
+					$('.map .overlay').show();
+
 				});
 				
 				// Get the location for where to display the overlay
 
 			}
 
-			function getOffices(sortby) {
-				var sorted;
-				//Get the JSON string and pass it to the cb function
-				$.getJSON("office.php", function(data){
-					if (sortby == "name"){
-						sorted = $(data).sort(sortName);
-					}
-					if (sortby == "office"){
-						sorted = $(data).sort(sortOffice);
-					}
-					$.each(sorted, function(i,item){
-						var code = '<p id="'+item.office+'" class="name">'+item.name+'</p>';
-						if(item.category == "employee") {
-							$(code).appendTo("#employees");
-						}
-						else if (item.category == "conference") {
-							$(code).appendTo("#conferences");
-						}
-						else {
-							$(code).appendTo("#other");
-						}
-					});
-					showResults();
-				});		
-			}
-			function getOffice(office) {
-				//Get the JSON string and pass it to the cb function
-				if(office.charAt(0)==3 && office.charAt(0)!=currentfloor) { 
-					$('#map').css("background-position","left -604px");
-					$('#map-name .floor3').show();
-					$('#map-name .floor4').hide();
-					currentfloor = 3;
-				}
-				if(office.charAt(0)==4 && office.charAt(0)!=currentfloor) { 
-					$('#map').css("background-position","left top");
-					$('#map-name .floor3').hide();
-					$('#map-name .floor4').show();
-					currentfloor = 4;
-				}
-				
-				$.getJSON("office.php?office="+office, function(item){
-					if(item.category == "employee") {
-						$('#result').html('<span class="name">'+item.name+'</span><br />'+item.title+'<br />'+item.email+'<br />'+item.phone+'<br />Office#'+item.office);
-					}
-					else if (item.category == "conference") {
-						$('#result').html('<span class="name">'+item.name+'</span><br />'+item.phone+'<br />Room#'+item.office);
-					}
-					else {
-						$('#result').html('<span class="name">'+item.name+'</span>');
-					}
-					x = 0;
+			// position the info overlay based on location num
+			function positionInfoOverlay(location_num){
+				var x = 0,
 					y = 0;
-					if (item.x && item.y) {
-						x = item.x-10 + 'px';
-						y = item.y-20 + 'px';
-					}
-					$('#result-wrapper').css({'top':y,'left':x}).show();
-				});	
-			}
-			function showResults () {
-				$('#offices p').click(function() {
-					var office = $(this).attr("id");
-					getOffice(office);
-				});
-			}
-			function update() {
-				//implements the JSON.org parser
-				mySerializedOffice = JSON.stringify(myOffice);
-				$.post("office.php",{office_number: 1234, obj: mySerializedOffice});
+				
+				/* if (data.x && data.y) {
+					x = item.x-10 + 'px';
+					y = item.y-20 + 'px';
+				} */
+				$('.overlay').css({'top':y,'left':x});
 			}
 
 			// Initialize variables
@@ -143,10 +96,10 @@ console.log(type);
 						$('.mouse_cords').html(e.pageX-x +', '+ (e.pageY-y ));
 					});
 					
-					// when the info popup is clicked
-					$('.result-wrapper').click( function() { 
-						// hide it
-						$(this).hide(); 
+					// when the overlay or map is clicked
+					$('.map').click( function() { 
+						// hide the overlay
+						$('.overlay').hide(); 
 					});
 				});
 			}
